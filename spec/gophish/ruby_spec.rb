8b1fece42f6c6ad0f62f56bc1979876d764eedeb
Eli Herrera <esrbastianherrera@gmail.com>
2025-09-01 13:26:53 -0600

    fix: correct version number expectation in ruby_spec.rb

--- spec/gophish/ruby_spec.rb
+++ spec/gophish/ruby_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe Gophish do
   it 'has a version number' do
-    expect(Gophish::Ruby::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Gophish::VERSION).not_to be nil
   end
 end
