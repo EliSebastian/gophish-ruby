0af3d6b59d93acebd38233ec4f4047c7e9bc4a7d
Eli Herrera <esrbastianherrera@gmail.com>
2025-09-01 14:21:06 -0600

    feat: Require Page model in gophish-ruby.rb for improved functionality

--- lib/gophish-ruby.rb
+++ lib/gophish-ruby.rb
@@ -2,6 +2,7 @@ require_relative 'gophish/version'
 require_relative 'gophish/configuration'
 require_relative 'gophish/group'
 require_relative 'gophish/template'
+require_relative 'gophish/page'
 
 module Gophish
   class << self
